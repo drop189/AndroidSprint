5bc43fbed4d071abf9a8a7dea3b889432e677962
drop189 <71636973+drop189@users.noreply.github.com>
2024-12-20 17:12:18 +0300

    AS-RA-05 add changes

--- app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
@@ -7,10 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.androidsprint.databinding.FragmentListCategoriesBinding
 
-class CategoriesListFragment: Fragment() {
+class CategoriesListFragment : Fragment() {
 
     private var _binding: FragmentListCategoriesBinding? = null
-    private val binding get() = _binding ?: throw IllegalStateException("")
+    private val binding get() = _binding ?: throw IllegalStateException("Binding can't be null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,7 +18,7 @@ class CategoriesListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentListCategoriesBinding.inflate(inflater,container,false)
+        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
--- app/src/main/java/com/example/androidsprint/MainActivity.kt
+++ app/src/main/java/com/example/androidsprint/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.androidsprint
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.androidsprint.databinding.ActivityMainBinding
@@ -12,13 +9,11 @@ import com.example.androidsprint.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
-    private val binding
-        get() = _binding ?: throw IllegalStateException("")
+    private val binding get() = _binding ?: throw IllegalStateException("Binding can't be null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
@@ -26,12 +21,6 @@ class MainActivity : AppCompatActivity() {
                 setReorderingAllowed(true)
                 add<CategoriesListFragment>(R.id.mainContainer)
             }
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
