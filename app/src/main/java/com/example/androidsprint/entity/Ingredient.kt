16c01b67dbc2dfb9f55d47c1ff44f6fbc8823a5f
drop189 <71636973+drop189@users.noreply.github.com>
2025-07-09 18:09:48 +0300

    AS-RA-07-1 done task

--- app/src/main/java/com/example/androidsprint/entity/Ingredient.kt
+++ app/src/main/java/com/example/androidsprint/entity/Ingredient.kt
@@ -1,7 +1,7 @@
 package com.example.androidsprint.entity
 
 data class Ingredient(
-    val quantity: Float,
+    val quantity: String,
     val unitOfMeasure: String,
     val description: String,
 )
